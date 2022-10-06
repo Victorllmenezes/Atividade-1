4e4aec2896e68028bae1528d361d67e88c9be56b
ketily <ketilyr@gmail.com>
2022-10-06 17:44:25 -0300

    separação das etapas

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("funcionou");
+const readline = require('readline-sync')
+
+
+// Receber input de n números no terminal.
+let numeros: number[] = [];
+    // loop para perguntar valores
+
+    const valor = readline.question("Digite um número: ");
+
+    //verificar se o valor é valido para salvar ou não na lista de numeros
+        numeros.push()
+    // forma do usuario sair do loop
+console.log(numeros);
+
+
+
+// Multiplicar todos os números.
+// Mostrar números no terminal.
+// Receber input de 1 numero no terminal.
+// Dividir o resultado anterior pelo número inserido.
+// Mostrar resultado no terminal.
+
