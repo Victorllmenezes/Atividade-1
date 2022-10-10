c37f59d89810dcf11204f0af8e67a34c6790c7e6
João Victor <Victor--menezes@hotmail.com>
2022-10-10 09:16:31 -0300

    * Implementado atividade base para mostrar aos estudantes após finalizarem.

--- src/server.ts
+++ src/server.ts
@@ -1,81 +1,8 @@
-const prompt = require("readline-sync");
+import Atividade1 from "./Atividade-1/Atividade-1";
+import { Perguntar } from "./Atividade-1/controller/PromptController";
 
-let numero: any;
-let listaDeNumeros: any = [];
-let resultadoDaMultiplicação = 1;
-let numeroDivisor;
-let resultadoFinal;
-let adicionarNumero: any;
-let escolhaDoUsuario: any = true;
-
-function validador(index: any) {
-  listaDeNumeros.push(numero);
-  index = isNaN(index);
-  if (index == false) {
-  } else {
-    switch (numero) {
-      case "e":
-        listaDeNumeros.pop();
-        break;
-
-      default:
-        console.log("Nao eh um numero!!");
-        listaDeNumeros.pop();
-        break;
-    }
-  }
+let executando = true;
+while (executando) {
+  Atividade1();
+  if (Perguntar("Para tentar novamente basta clicar no Enter! (N para abortar)").toUpperCase() == "N") executando = false;
 }
-
-//Receber input de n números no terminal.
-
-while (escolhaDoUsuario == true) {
-  numero = prompt.question("Digite um numero ou (e) para sair: ");
-  validador(numero);
-  if (numero == "e" || numero == "E") {
-    escolhaDoUsuario = false;
-  } else {
-  }
-}
-
-//Multiplicar todos os números.
-for (let index = 0; index < listaDeNumeros.length; index++) {
-  resultadoDaMultiplicação = resultadoDaMultiplicação * listaDeNumeros[index];
-}
-
-//Mostrar números no terminal.
-console.log(listaDeNumeros);
-
-//Receber input de 1 numero no terminal.
-do {
-  numeroDivisor = prompt.question("Digite um numero divisor: ");
-  if (numeroDivisor == 0) {
-    console.log("Impossivel dividir por 0");
-  } else {
-    //Dividir o resultado anterior pelo número inserido.
-    resultadoFinal = resultadoDaMultiplicação / numeroDivisor;
-  }
-} while (numeroDivisor == 0);
-
-//Mostrar resultado no terminal.
-console.log(resultadoFinal);
-
-// Receber input de n números no terminal.
-//Loop com uma saida
-//Perguntar numero
-//Validar o numero
-//Armazenar o numero
-//Fim do loop
-
-// Multiplicar todos os números.
-//Percorrer a lista multiplicando valores
-
-// Mostrar números no terminal.
-
-// Receber input de 1 numero no terminal.
-//Perguntar numero
-//Validar o numero
-//Armazenar o numero
-
-// Dividir o resultado anterior pelo número inserido.
-
-// Mostrar resultado no terminal.
