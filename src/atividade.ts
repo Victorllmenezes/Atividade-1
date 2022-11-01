84e84f80e1b7b34dfd3b044ab31fcf17fdd60d7a
João Victor <Victor--menezes@hotmail.com>
2022-11-01 09:05:45 -0300

    * Alterado testes unitarios.

--- src/atividade.ts
+++ src/atividade.ts
@@ -1,6 +1,3 @@
 export function maiorNumeroNaLista(numeros: number[]): { maior: number; indice: number } {
-  const maiorNumero: number = numeros.reduce(function (anterior: number, atual: number) {
-    return anterior > atual ? anterior : atual;
-  });
-  return maiorNumero;
+  throw Error("Not implemented");
 }
