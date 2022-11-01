f5672ff6e7d3df4b6b81854865ab25f9957c477a
João Victor <Victor--menezes@hotmail.com>
2022-11-01 08:49:59 -0300

    * Parte 2 da atividade 1/2

--- src/atividade.ts
+++ src/atividade.ts
@@ -1,8 +1,5 @@
-export function maiorNumeroNaLista(numeros: number[]): number {
-  const maiorNumero: number = numeros.reduce(function (
-    anterior: number,
-    atual: number
-  ) {
+export function maiorNumeroNaLista(numeros: number[]): { maior: number; indice: number } {
+  const maiorNumero: number = numeros.reduce(function (anterior: number, atual: number) {
     return anterior > atual ? anterior : atual;
   });
   return maiorNumero;
