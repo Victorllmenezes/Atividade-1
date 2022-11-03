771fe96cbf7647ada16d0a177954471b6807f9eb
ketily <ketilyr@gmail.com>
2022-11-03 14:54:37 -0300

    valoreindice

--- src/atividade.ts
+++ src/atividade.ts
@@ -1,15 +1,11 @@
-<<<<<<< HEAD
-export function maiorNumeroNaLista(numeros: number[]): number {
-  
-  return numeros.reduce(function(prev, current) { 
-    return prev > current ? prev : current; 
-  });
-  
-=======
 export function maiorNumeroNaLista(numeros: number[]): { maior: number; indice: number } {
-  const maiorNumero: number = numeros.reduce(function (anterior: number, atual: number) {
-    return anterior > atual ? anterior : atual;
-  });
-  return maiorNumero;
->>>>>>> atividade2
+let maior = 0;
+let indice = 0;
+    for (let i = 0; i < numeros.length; i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+            indice = i;
+        }
+    }
+    return { maior, indice };  
 }
