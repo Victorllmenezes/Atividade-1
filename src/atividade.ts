99f7d6e6199c86bdb2f27d43346fb4abcdff08ec
João Victor <Victor--menezes@hotmail.com>
2022-11-08 11:01:35 -0300

    * COntinuação das atividades...

--- src/atividade.ts
+++ src/atividade.ts
@@ -1,3 +1,19 @@
 export function maiorNumeroNaLista(numeros: number[]): { maior: number; indice: number } {
   throw Error("Not implemented");
 }
+
+/**
+ * Juntar n listas e ordena-las
+ * @param listas Lista de listas de numeros
+ */
+export function juntarListasOrdenando(listas: number[][]): number[] {
+  throw Error("Not implemented");
+}
+
+/**
+ * Tirar a media da soma das listas
+ * @param listas Lista de listas de numeros
+ */
+export function mediaDaSomaDasListas(listas: number[][]): number {
+  throw Error("Not implemented");
+}
