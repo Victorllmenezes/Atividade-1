3552ac708b397db7c4949091dc8a4b1bf70838df
João Victor <Victor--menezes@hotmail.com>
2022-11-10 10:17:08 -0300

    * Implementado nova atividade para estudos

---
+++ src/segundosToDate.ts
@@ -0,0 +1,9 @@
+/**
+ * Recebe um valor em segundos e converte para anos, dias, horas, minutos e segundos,
+ * assumindo que 1 ano tem 365 dias, 1 dia tem 24 horas, 1 hora tem 60 minutos e 1 minuto tem 60 segundos
+ * @param segundos Valor em segundos
+ * @returns Objeto com os valores em anos, dias, horas, minutos e segundos
+ */
+export function SegundosParaEstruturaDeData(segundos: number): { anos: number; dias: number; horas: number; minutos: number; segundos: number } {
+  throw Error("Not implemented");
+}
