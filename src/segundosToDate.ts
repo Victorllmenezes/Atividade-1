0fa2e85ad74435350874d5543c8e10fa48a2e62a
Gabriel Zacarias <gabrielzsdev@gmail.com>
2022-11-10 13:09:14 -0300

    Atividade para correção

--- src/segundosToDate.ts
+++ src/segundosToDate.ts
@@ -1,3 +1,5 @@
+import { match } from "assert";
+
 /**
  * Recebe um valor em segundos e converte para anos, dias, horas, minutos e segundos,
  * assumindo que 1 ano tem 365 dias, 1 dia tem 24 horas, 1 hora tem 60 minutos e 1 minuto tem 60 segundos
@@ -5,11 +7,17 @@
  * @returns Objeto com os valores em anos, dias, horas, minutos e segundos
  */
 export function SegundosParaEstruturaDeData(segundos: number): { anos: number; dias: number; horas: number; minutos: number; segundos: number } {
+  const minutosEmSegundos = 60;
+  const horasEmSegundos = minutosEmSegundos * 60;
+  const diasEmSegundos = horasEmSegundos * 24;
+  const anosEmSegundos = diasEmSegundos * 365;
+  console.log("Teste:" + anosEmSegundos);
+
   return {
-    anos: Math.floor(segundos / 3.154e7),
-    dias: Math.floor((segundos % 3.154e7) / 86400),
-    horas: Math.floor((segundos % 86400) / 3600),
-    minutos: Math.floor((segundos % 3600) / 60),
+    anos: Math.floor(segundos / anosEmSegundos),
+    dias: Math.floor((segundos % anosEmSegundos) / diasEmSegundos),
+    horas: Math.floor((segundos % diasEmSegundos) / horasEmSegundos),
+    minutos: Math.floor((segundos % horasEmSegundos) / minutosEmSegundos),
     segundos: segundos % 60,
   };
 }
