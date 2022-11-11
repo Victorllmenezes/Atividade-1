e615c6ed97e0409a9587e3f96fcb266c60758877
ketily <ketilyr@gmail.com>
2022-11-11 15:01:16 -0300

    data

--- src/segundosToDate.ts
+++ src/segundosToDate.ts
@@ -4,6 +4,13 @@
  * @param segundos Valor em segundos
  * @returns Objeto com os valores em anos, dias, horas, minutos e segundos
  */
-export function SegundosParaEstruturaDeData(segundos: number): { anos: number; dias: number; horas: number; minutos: number; segundos: number } {
-  throw Error("Not implemented");
+export function SegundosParaEstruturaDeData(segundos: number): { anos: number; dias: number; 
+  horas: number; minutos: number; segundos: number } {
+   const minutos = 60/60
+   const horas = 3600/60
+   const dias = 86400/24
+  const anos = 31536000/365
+
+  return {anos: Math.floor(anos), dias: Math.floor(dias % 24), 
+  horas: Math.floor(horas % 24), minutos: Math.floor(minutos % 60), segundos: Math.floor(segundos % 60)}
 }
