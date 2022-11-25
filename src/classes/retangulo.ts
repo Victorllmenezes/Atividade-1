9a9e5c1fb60d8f02174be34efd9e8842aa843cc9
Gabriel Zacarias <gabrielzsdev@gmail.com>
2022-11-25 13:54:35 -0300

    Resolução parcial

--- src/classes/retangulo.ts
+++ src/classes/retangulo.ts
@@ -40,19 +40,21 @@ export class Retangulo {
   }
   //Implementar método estadoEmRelacaoA
   estadoEmRelacaoA(_objeto: Retangulo): estadoInteracao {
-    const verticeEsquerdaCima = this.estaDentro(_objeto.x1, _objeto.y1);
-    const verticeDireitaBaixo = this.estaDentro(_objeto.x2, _objeto.y2);
-    const verticeDireitaCima = this.estaDentro(_objeto.x2, _objeto.y1);
-    const verticeEsquerdaBaixo = this.estaDentro(_objeto.x1, _objeto.y2);
+    const figura1verticeEsquerdaCima = this.estaDentro(_objeto.x1, _objeto.y1);
+    const figura1verticeDireitaBaixo = this.estaDentro(_objeto.x2, _objeto.y2);
+    const figura1verticeDireitaCima = this.estaDentro(_objeto.x2, _objeto.y1);
+    const figura1verticeEsquerdaBaixo = this.estaDentro(_objeto.x1, _objeto.y2);
 
-    console.log(verticeDireitaBaixo, verticeDireitaCima, verticeEsquerdaBaixo, verticeEsquerdaCima);
-    const test1 = _objeto.x1 >= this.x1;
+    const figura2verticeEsquerdaCima = _objeto.estaDentro(this.x1, this.y1);
+    const figura2verticeDireitaBaixo = _objeto.estaDentro(this.x2, this.y2);
+    const figura2verticeDireitaCima = _objeto.estaDentro(this.x2, this.y1);
+    const figura2verticeEsquerdaBaixo = _objeto.estaDentro(this.x1, this.y2);
 
-    if (verticeEsquerdaCima && verticeDireitaCima && verticeEsquerdaBaixo && verticeDireitaBaixo) {
+    if (figura1verticeEsquerdaCima && figura1verticeDireitaCima && figura1verticeEsquerdaBaixo && figura1verticeDireitaBaixo) {
       return estadoInteracao.contido;
-    } else if (verticeEsquerdaCima || verticeDireitaCima || verticeEsquerdaBaixo || verticeDireitaBaixo) {
+    } else if (figura1verticeEsquerdaCima || figura1verticeDireitaCima || figura1verticeEsquerdaBaixo || figura1verticeDireitaBaixo) {
       return estadoInteracao.colidindo;
-    } else if (test1) {
+    } else if (figura2verticeDireitaBaixo && figura2verticeDireitaCima && figura2verticeEsquerdaBaixo && figura2verticeEsquerdaCima) {
       return estadoInteracao.contendo;
     } else {
       return estadoInteracao.naoColidindo;
